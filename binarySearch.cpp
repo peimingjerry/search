67de0bfd199e32c098ade02f217c293a5fea573f
peimingjerry <zhoupeiming@qq.com>
2016-04-05 15:46:25 +0800

    Create binarySearch.cpp

---
+++ binarySearch.cpp
@@ -0,0 +1,85 @@
+
+#include "person.h"
+#include "binarySearch.h"
+
+#define SIZE 10
+
+int main() {
+    BinarySearch<Person, long> myBinarySearch;
+    Person *persons[SIZE];
+
+    // Construct tree
+    persons[0] = new Person(1050, "A", 0);
+    persons[1] = new Person(1080, "B", 0);
+    persons[2] = new Person(1110, "C", 0);
+    persons[3] = new Person(1000, "D", 0);
+    persons[4] = new Person(1008, "E", 0);
+    persons[5] = new Person(1103, "F", 0);
+    persons[6] = new Person(1001, "G", 0);
+    persons[7] = new Person(1002, "H", 0);
+    persons[8] = new Person(1012, "I", 0);
+    persons[9] = new Person(1102, "J", 0);
+
+    for (int i=0;i<SIZE;i++) {
+        persons[i]->display();
+        myBinarySearch.insert(*persons[i]);
+    }
+    cout<<endl;
+
+    for (int i=0;i<SIZE;i++)
+        delete persons[i];
+
+    myBinarySearch.traversal();
+    cout<<endl;
+
+    // Search
+    Person *result;
+    long key;
+    key = 1012;
+    cout<<"Searching element with key="<<key<<"..."<<endl;
+    result = myBinarySearch.search(key);
+    if (result != NULL) {
+        cout<<"Found!"<<endl;
+        result->display();
+    } else
+        cout<<"NOT Found!"<<endl;
+    cout<<endl;
+
+    key = 1013;
+    cout<<"Searching element with key="<<key<<"..."<<endl;
+    result = myBinarySearch.search(key);
+    if (result != NULL) {
+        cout<<"Found!"<<endl;
+        result->display();
+    } else
+        cout<<"NOT Found!"<<endl;
+    cout<<endl;
+
+    key = 1050;
+    cout<<"Removing element with key="<<key<<"..."<<endl;
+    myBinarySearch.remove(key);
+    cout<<endl;
+
+    myBinarySearch.traversal();
+    cout<<endl;
+
+    key = 1008;
+    cout<<"Removing element with key="<<key<<"..."<<endl;
+    myBinarySearch.remove(key);
+    cout<<endl;
+
+    myBinarySearch.traversal();
+    cout<<endl;
+
+    key = 1050;
+    cout<<"Searching element with key="<<key<<"..."<<endl;
+    result = myBinarySearch.search(key);
+    if (result != NULL) {
+        cout<<"Found!"<<endl;
+        result->display();
+    } else
+        cout<<"NOT Found!"<<endl;
+    cout<<endl;
+
+    return 0;
+}
